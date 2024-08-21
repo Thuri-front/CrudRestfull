25cfaa9574527e0664d2f8193970c54250561ecd
Thuri <132075721+Thuri-front@users.noreply.github.com>
2024-08-21 13:16:40 -0300

    ClienteService
    Adiocionado funcoes return ao Cliente

--- src/main/java/service/ClienteService.java
+++ src/main/java/service/ClienteService.java
@@ -1,12 +1,12 @@
 package service;
 
-
 import model.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.ClienteRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -23,11 +23,19 @@ public class ClienteService {
     }
 
     public Cliente buscarClientePorId(Long id) {
+        Optional<Cliente> clienteOpt = clienteRepository.findById(id);
+        return clienteOpt.orElse(null); // Retorna null se o cliente não for encontrado
     }
 
     public Cliente atualizarCliente(Long id, Cliente cliente) {
+        if (clienteRepository.existsById(id)) {
+            cliente.setiD(id);
+            return clienteRepository.save(cliente);
+        }
+        return null; // Retorna null se o cliente não existir
     }
 
     public void deletarCliente(Long id) {
+        clienteRepository.deleteById(id);
     }
 }
